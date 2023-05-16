9fa3f19a7be076d2cd9b306cfe3e2218db2fc9e2
tozsitek <tozsitek@gmail.com>
2023-05-16 15:23:22 -0400

    Create style.py

---
+++ edxIntro/style.py
@@ -0,0 +1,7 @@
+# url: https://pycodestyle.pycqa.org/en/latest/
+# PEP 8 - python enhancement program
+# pylint is a styling tool
+# pycodestyle - code formatter
+# black - code formatter: https://black.readthedocs.io/en/stable/
+
+# to run it: black style.py
