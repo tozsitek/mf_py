eeabde03252cf05580b72a6019c12218d055d985
tozsitek <tozsitek@gmail.com>
2023-05-17 15:23:14 -0400

    Update test_calculator.py

--- edxIntro/unitTests/test_calculator.py
+++ edxIntro/unitTests/test_calculator.py
@@ -1,9 +1,14 @@
 from calculator import square
+import pytest
 
-def main():
-    test_square()
+# use unit test
+# use pytest: pip install pytest
+# url: docs.pytest.org
 
-def test_square():
+#def main():
+#    test_square()
+
+""" def test_square():
     #if square(2) != 4:
     #    print("2 squared was not 4")
     #if square(3) != 9:
@@ -27,8 +32,25 @@ def test_square():
     try:
         assert square(0) == 0
     except AssertionError:
-        print("0 squared was not 0")
+        print("0 squared was not 0") """
 
+# if __name__ == "__main__":
+#    main()
 
-if __name__ == "__main__":
-    main()
+### use pytest version
+# to run it on CLI: pytest test_calculator.py
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+def test_zero():
+    assert square(0) == 0
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
